7990abf94411ce7abf593da85fa30dbe1d851bec
Jonathan Monroy <jmonroybat@gmail.com>
2022-06-20 21:35:03 -0600

    iniciando con el jwt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
@@ -16,4 +17,23 @@ const userSchema = new Schema({
   },
 });
 
-export const User = model("user", userSchema);
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(user.password, salt);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Falló el hash de contraseña");
+  }
+});
+
+userSchema.methods.comparePassword = async function (canditatePassword) {
+  return await bcryptjs.compare(canditatePassword, this.password);
+};
+
+export const User = model("User", userSchema);
